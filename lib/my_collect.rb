0583da0af793824b24b7a6699399f3b18bfd5031
Marvin Reyes <reyemarv@gmail.com>
2021-01-27 05:59:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,13 @@
 def my_collect(array)
   i = 0 
+  new_array = []
   
-  
-  while i < array.length 
+  while i < array.length
+  new_array << yield(array[i])
+  i += 1 
   end 
+  
+  new_array
   
 end 
 
